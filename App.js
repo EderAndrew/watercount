6e40bdaedc9955da6530bbc1caf5d4db93b32de1
Eder Andrew <ederandrew0028@gmail.com>
2020-01-04 17:53:27 -0300

    Configuration of visual part of app

--- App.js
+++ App.js
@@ -1,114 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { useState } from 'react'
+import styled from 'styled-components/native'
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+const Container = styled.SafeAreaView`
+  flex:1;
+`
+const ImageBack = styled.ImageBackground`
+  flex:1;
+  width:100%;
+  align-items:center;
+`
+const Header = styled.View`
+  width:100%;
+  height:80px;
+  justify-content:center;
+  align-items:center;
+`
+const Title = styled.Text`
+  font-size:30px;
+  font-weight:bold;
+  color:#45b0fb;
+`
+const ContainerInfo = styled.View`
+  width:100%;
+  height:120px;
+  flex-direction:row;
+  align-items:center;
+  justify-content:space-between;
+  padding:20px;
+`
+const Area = styled.View`
+  justify-content:center;
+  align-items:center;
+`
+const TextArea = styled.Text`
+  font-size:18px;
+  color:#45b0fb;
+  font-weight:bold;
+`
+const MetaText = styled.Text`
+  font-size:26px;
+  color:#45b0fb;
+`
+const ConsuText = styled.Text`
+  font-size:26px;
+  color:#45b0fb;
+`
+const StatusText = styled.Text`
+  font-size:26px;
+  color:#45b0fb;
+`
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const ContainerConsumo = styled.View`
+  width:100%;
+  height:140px;
+  justify-content:center;
+  align-items:center;
+  margin-top:90px;
+`
+const Quantidade = styled.Text`
+  font-size:80px;
+  font-weight:bold;
+  color:#FFF;
+`
+const ButtonCount = styled.TouchableOpacity`
+  width:250px;
+  height:60px;
+  justify-content:center;
+  align-items:center;
+  border:2px solid #FFF;
+  margin-top:150px;
+  border-radius:25px;
+`
+const TitleButton = styled.Text`
+  color:#FFF;
+  font-size:18px;
+`
+const App = () => {
+  const [meta, setMeta] = useState(2)
+  const [consumo, setConsumo] = useState(0)
+  const [status, setStatus] = useState(0)
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+  return(
+    <Container>
+      <ImageBack source={require('./src/imgs/waterbg.png')}>
+        <Header>
+        <Title>Drink Water App</Title>
+        </Header>
+        <ContainerInfo>
+          <Area>
+            <TextArea>Meta</TextArea>
+            <MetaText>{ meta }L</MetaText>
+          </Area>
+          <Area>
+            <TextArea>Consumido</TextArea>
+            <ConsuText>{ consumo }L</ConsuText>
+          </Area>
+          <Area>
+            <TextArea>Status</TextArea>
+            <StatusText>{ status }%</StatusText>
+          </Area>
+        </ContainerInfo>
+        <ContainerConsumo>
+          <Quantidade>0</Quantidade>
+        </ContainerConsumo>
+        <ButtonCount>
+          <TitleButton>Beber</TitleButton>
+        </ButtonCount>
+      </ImageBack>
+    </Container>
+  )
+}
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+export default App
